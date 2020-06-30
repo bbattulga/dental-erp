5e2d443cf6a4c4b7b5e4ebdc64e653b0cce010e0
= <battulga.dev@gmail.com>
2020-07-01 02:48:21 +0800

    fixed User.php

--- app/User.php
+++ app/User.php
@@ -5,10 +5,8 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 
-
-class User extends Model
+class User extends Authenticatable
 {
     use Notifiable;
 

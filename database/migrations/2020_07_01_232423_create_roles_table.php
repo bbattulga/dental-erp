c99005a4ccfc70205c13bd02bebc91c35eba3fe9
= <battulga.dev@gmail.com>
2020-07-02 01:53:17 +0800

    added some random 2 migration files before refactoring migration files

--- database/migrations/2020_07_01_232423_create_roles_table.php
+++ database/migrations/2020_07_01_232423_create_roles_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateRolesTable extends Migration
 {
@@ -15,15 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
-            $table->integer('role_id');
-            $table->tinyInteger('state');
-            //Admin->0
-            //Reception->1
-            //Doctor->2
-            //Assistant->3
-            //Accountant->4
-            //Others->5
+            $table->string('name');
             $table->timestamps();
         });
     }

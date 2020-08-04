06a1543d7138dfc6ba76a349386069f3e5d56765
= <battulga.dev@gmail.com>
2020-08-04 11:55:03 +0800

    manual merge toothtype model

--- app/ToothType.php
+++ app/ToothType.php
@@ -10,9 +10,31 @@ class ToothType extends Model
 
     protected $table='tooth_types';
 
+    private static $MILK;
+    private static $PERMA;
+
     public function getShortNameAttribute(){
     	$name = $this->name;
     	$words = explode(' ', $name);
     	return mb_strtolower($words[0]);
+    }
+
+    public static function milk(){
+
+    	if (!isset(self::$MILK)){
+    		self::$MILK = self::where('name', 'like', '%Сү%')
+    				->orWhere('name', 'like', '%Milk%')
+    				->first();
+    	}
+    	return self::$MILK;
+    }
+
+    public static function permanent(){
+    	if (!isset(self::$PERMA)){
+    		self::$PERMA = self::where('name', 'like', '%Яс%')
+    				->orWhere('name', 'like', '%Perm%')
+    				->first();
+    	}
+    	return self::$PERMA;
     }
 }

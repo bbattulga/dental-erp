5072d870e70e7635fd9ff33029e2bde881edfa6f
= <battulga.dev@gmail.com>
2020-08-09 14:11:19 +0800

    changed page title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Diverse Solution</title>
+    <title>Diverse Solutions</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="{{asset('font/iconsmind/style.css')}}" />
